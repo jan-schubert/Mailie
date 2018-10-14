2dd81b3ba0bcd53efc89763f43c38c45b14111b1
Jan Schubert <dev@schubert-jan.de>
2018-10-14 13:08:05 +0200

    Added attach property for embedded images.

---
+++ Source/Mailie/Mailie/Mailie/Mvvm/EmbeddedImage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mailie.Mvvm
+{
+  [ContentProperty(nameof(Source))]
+  public class ImageResourceExtension : IMarkupExtension
+  {
+    public string Source { get; set; }
+
+    public object ProvideValue(IServiceProvider serviceProvider)
+    {
+      return Source == null ? null : ImageSource.FromResource(Source, typeof(ImageResourceExtension).GetTypeInfo().Assembly);
+    }
+  }
+}
